f1dcee68a3efc316271afe842c167c1be3ee5c8a
Yuliap21 <yulia777best@gmail.com>
2021-08-21 13:32:10 -0400

    css added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,19 @@
 import React, { useState } from 'react';
 
 export default function Home(props) {
-	return <div className="HomePage">This is the {props.page} page</div>;
+	return (
+		<div id="notesWrapper">
+			<div class="notes">
+				<div class="note">
+					<div class="note-header">
+						<span class="note-close">
+							<i class="fas fa-times"></i>
+						</span>
+					</div>
+					<div class="noteTitle">Title goes here</div>
+					<div class="noteBody">Body goes here</div>
+				</div>
+			</div>
+		</div>
+	);
 }
